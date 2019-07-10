6b6c6354bb43dc809f9b94d1f74049a5d9c7c026
Megan Grant <megangrant@ymail.com>
2019-07-10 14:12:30 -0400

    Lab completed with errors to discuss in 1-1 07/10/2019

--- school.py
+++ school.py
@@ -1,1 +1,18 @@
-class School:
+class School():
+    def __init__(self, name, grade = 0):
+        self.name = name
+        self.roster = {}
+        self.grade = grade
+    def add_student(self, name, grade):
+        if grade in self.roster:
+            self.roster[grade].append(name)
+        else:
+            self.roster[grade] = [name]
+    def grade(self, grade):
+        return self.roster[grade]
+    def sort_roster(self):
+        roster_sort = sorted(self.roster, key = lambda x: x.grade, reverse=True)
+        for c in roster_sort:
+            print(c.grade, c.name)
+            
+            
